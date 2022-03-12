435f518d8509f1a7270ace33a0bc0c9d65862627
mihailname <101433444+mihailname@users.noreply.github.com>
2022-03-12 14:05:31 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -13,8 +13,12 @@ print("GRAB - Started")
 
 @client.on(events.NewMessage(chats=channels))
 async def my_event_handler(event):
-    if event.message:
-        await client.send_message(my_channel_id, file=event.messages, message=event.original_update.message.message,)
-
+    if event.message.text:
+        await client.send_message(my_channel_id, event.message)
+@client.on(events.Album(chats=channels))
+async def handler(event):
+    if event.message.media:
+    await client.send_message(my_channel_id, file=event.messages, message=event.original_update.message.message,)
+ 
 client.start()
 client.run_until_disconnected()
